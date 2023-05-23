6d9f1d5778fe24722d987d6e72e69d293458e01e
Matthew Gast <incendium@gmail.com>
2023-05-22 22:07:42 -0500

    Attempt to auto-deploy dokka pages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.plugin.serialization)
-    // alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka)
     alias(libs.plugins.detekt)
     alias(libs.plugins.versions)
     alias(libs.plugins.versionsFilter)
